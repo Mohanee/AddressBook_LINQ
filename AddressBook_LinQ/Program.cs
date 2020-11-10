c6048a0021035a73a813729170cebeb4e2952504
Mohanee <mohanee99@gmail.com>
2020-11-10 10:00:15 +0530

    [mohanee]Add .gitignore file

---
+++ AddressBook_LinQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_LinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
