109084a2c67e9cc0d64df8eecf72cb67034bacb7
Mohanee <mohanee99@gmail.com>
2020-11-10 19:34:56 +0530

    [mohanee]Add data columns to the data table

--- AddressBook_LinQ/Linq_Operations.cs
+++ AddressBook_LinQ/Linq_Operations.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
 
 namespace AddressBook_LinQ
 {
-    class Linq_Operations
+    public class Linq_Operations
     {
+        // Create Data table to store address book details
+        public DataTable dataTable = new DataTable();
+
+        /// <summary>
+        /// Add columns to the created data table
+        /// </summary>
+        public void CreateAddressBook()
+        {
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("Zipcode", typeof(string));
+            dataTable.Columns.Add("PhoneNumber", typeof(string));
+            dataTable.Columns.Add("EmailID", typeof(string));
+        }
     }
 }
--- AddressBook_LinQ/Program.cs
+++ AddressBook_LinQ/Program.cs
@@ -6,7 +6,9 @@ namespace AddressBook_LinQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Welcome to AddressBook DataTable Using LinQ");
+            Linq_Operations lopr = new Linq_Operations();
+            lopr.CreateAddressBook();
         }
     }
 }
